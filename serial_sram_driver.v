2baa6e7c2146d34749978a857dbd3996134ff63e
jmstein7 <79536523+jmstein7@users.noreply.github.com>
2021-10-23 08:51:21 -0400

    Add files via upload

--- serial_sram_driver.v
+++ serial_sram_driver.v
@@ -1,22 +1,34 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer: Jonathan Stein
 // 
 // Create Date: 10/16/2021 03:46:22 PM
 // Design Name: 
 // Module Name: serial_sram_driver
 // Project Name: 
-// Target Devices: 
+// Target Devices: Xilinx Arty A7 or other FPGAs or ASICs
 // Tool Versions: 
-// Description: 
+// Description: Wrapper for Serial SRAM Driver
 // 
-// Dependencies: 
-// 
-// Revision:
+// Dependencies: Serial SRAM Driver
+// By Jonathan Stein
+// Copyright (C) 2021
+// Revision: 1.00
 // Revision 0.01 - File Created
 // Additional Comments:
-// 
+//   This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //////////////////////////////////////////////////////////////////////////////////
 
 
--- ssram_top.sv
+++ ssram_top.sv
@@ -7,16 +7,28 @@
 // Design Name: Quad SPI driver for 23LC512 Serial SRAM
 // Module Name: ssram_top
 // Project Name: 
-// Target Devices: 
+// Target Devices: Xilinx Arty A7 or other FPGAs or ASICs
 // Tool Versions: 
-// Description: 
+// Description: Serial SRAM Driver
 // 
 // Dependencies: 
-// 
-// Revision:
+// By Jonathan Stein
+// Copyright (C) 2021
+// Revision: 1.00
 // Revision 0.01 - File Created
 // Additional Comments:
-// 
+//   This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //////////////////////////////////////////////////////////////////////////////////
 
 
